81b498ba23563e1b26ccb3ec4cef971f26989e9f
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-20 18:05:47 -0300

    feat: add Catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,27 @@
-export default function CatalogPage() {
-  return <div>CatalogPage</div>
+import { Badge } from '@/components/ui/badge'
+import { prismaClient } from '@/lib/prisma'
+import { Shapes } from 'lucide-react'
+import { CategoryItem } from './components/category-item'
+
+export default async function CatalogPage() {
+  const categories = await prismaClient.category.findMany()
+
+  return (
+    <main className="px-6">
+      <section className="space-y-8">
+        <Badge
+          variant="outline"
+          className="px-3 py-1.5 border-2 font-bold uppercase"
+        >
+          <Shapes className="w-5 h-5 mr-1.5" />
+          Catálogo
+        </Badge>
+        <div className="grid grid-cols-2 gap-8">
+          {categories.map((category) => (
+            <CategoryItem key={category.id} category={category} />
+          ))}
+        </div>
+      </section>
+    </main>
+  )
 }
