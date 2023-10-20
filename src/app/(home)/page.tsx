1ddaae0e400cc769a16e01891de996324022bbfb
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-20 14:45:30 -0300

    feat: add first banner promo to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return <h2>teste</h2>
+  return (
+    <main>
+      <section>
+        <Image
+          src="/banner-home-01.png"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          alt="Até 50% de desconto só esse mês"
+        />
+      </section>
+    </main>
+  )
 }
