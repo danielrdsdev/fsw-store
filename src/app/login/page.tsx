2c6b84e3aa7604be373d2dfc5cb8203b5ab6998b
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-22 15:04:36 -0300

    feat: modified login page to be accessed only if not authenticated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Icons } from '@/components/shared/icons'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,25 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { toast } from '@/components/ui/use-toast'
-import { signIn, useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
+import { signIn } from 'next-auth/react'
 
 export default function LoginPage() {
-  const { status } = useSession()
-
-  if (status === 'authenticated') {
-    redirect('/')
-  }
-
-  const handleLoginClick = (provider: string) => {
-    signIn(provider)
-    toast({
-      title: 'Login feito com sucesso!',
-      description: 'Você será redirecionado em instantes',
-    })
-  }
-
   return (
     <main className="flex items-center justify-center pt-56 pb-16">
       <Card className="w-full max-w-md">
@@ -39,11 +21,11 @@ export default function LoginPage() {
         </CardHeader>
 
         <CardContent className="grid grid-cols-2 gap-4">
-          <Button onClick={() => handleLoginClick('google')} variant="outline">
+          <Button onClick={() => signIn('google')} variant="outline">
             <Icons.google className="w-6 h-6 mr-2" />
             Google
           </Button>
-          <Button onClick={() => handleLoginClick('github')} variant="outline">
+          <Button onClick={() => signIn('github')} variant="outline">
             <Icons.github className="w-6 h-6 mr-2" />
             GitHub
           </Button>
