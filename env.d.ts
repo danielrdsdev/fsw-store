63a8f977fd2b3a481d9d2ca2354e2bcbea665d6a
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-24 10:12:04 -0300

    feat: add stripe webhook

--- env.d.ts
+++ env.d.ts
@@ -7,6 +7,7 @@ declare namespace NodeJS {
     GITHUB_CLIENT_SECRET: string
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string
     STRIPE_SECRET_KEY: string
+    STRIPE_WEBHOOK_SECRET_KEY: string
     HOST_URL: string
   }
 }
