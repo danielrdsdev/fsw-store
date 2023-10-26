27a3c9a1724cd2102e0ed353423ee9cea97b5dd7
Daniel Rodrigues <kayque_daniel@hotmail.com>
2023-10-26 15:10:25 -0300

    fix: rollback config next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: 'fsw-store.s3.sa-east-1.amazonaws.com',
-      },
-      {
-        hostname: 'utfs.io',
-      },
-      {
-        hostname: 'i.imgur.com',
-      },
-    ],
+    domains: ['fsw-store.s3.sa-east-1.amazonaws.com', 'utfs.io', 'i.imgur.com'],
+  },
+  experimental: {
+    serverActions: true,
   },
 }
 
